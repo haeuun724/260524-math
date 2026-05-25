45f6e56a0b1928fbc02e7a7577aba3a2db9ee664
haeuun724 <graceshim7096@naver.com>
2026-05-25 08:35:28 +0000

    순열 및 계승 개념 추가

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,12 @@ import streamlit as st
 st.set_page_config(page_title="순열을 통한 문제 풀이", layout="wide")
 
 st.title("순열을 통한 문제 풀이")
-st.write("왼쪽 사이드바에서 원하는 페이지를 선택하여 학습을 시작하세요.")
+st.subheader("1. 순열이란?")
+st.write("- 서로 다른 n개에서 r(0<r<=n)개를 택하여 일렬로 나열하는 것은 n개에서 r개를 택하는 순열")
+st.write("- nPr = n(n-1) x ... x (n-r+1)")
+
+st.subheader("2. 계승이란?")
+st.write("- nPn = n(n-1) x ... x 2 x 1, 1부터 n까지의 자연수를 차례로 곱한 것 = n의 계승 = n!")
+st.write("- nP0 = 1")
+st.write("- 0! = 1")
+st.write("- nPr = n! / (n-r)! (단, 0<=r<=n)")
